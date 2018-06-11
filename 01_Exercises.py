3c087bf5a7ca8c9d1139c4efd06712150c76f5e3
Lynne Maher <lynnemaher007@gmail.com>
2018-06-11 17:10:52 -0400

    Initial Commit for uDemy REST API course.

---
+++ 01_Exercises.py
@@ -0,0 +1,25 @@
+# Create two variables, var1 and var2, both with the same value.
+var1 = 2
+var2 = 2
+
+
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 2
+num2 = 8
+print(num1*num2)
+
+
+# Complete the method by making sure it returns 42. .
+def return_42():
+    # Complete method here
+    return 42
+
+print(return_42())
+
+# Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+
+def my_method(arg1,arg2):
+    return arg1*arg2
+
+print(my_method(10,2))
