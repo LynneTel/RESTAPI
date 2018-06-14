957aa4a24d4e77a892cd4d86257b735ea92383ac
Lynne Maher <lynnemaher007@gmail.com>
2018-06-14 14:28:45 -0400

    Finished section: A Full Python Refresher

--- 11_PassingFunctions.py
+++ 11_PassingFunctions.py
@@ -6,24 +6,35 @@ def add_two_numbers():
     return 35 + 77
 
 
-methodception(add_two_numbers)
+# print(methodception(add_two_numbers))
 
 ###
 
-methodception(lambda:35 + 77)
+# print(methodception(lambda:35 + 77))
 
 my_list = [13, 56, 77, 484]
-list(filter(lambda x:x != 13,
-            my_list))  # A lambda function is just a short, one-line function that has no name.
+# my_list.remove(13)
+# print(my_list)
 
+# print(list(filter(lambda x:x != 13, my_list)))  # A lambda function is just a short, one-line function that has no name.
+# print(my_list)
 
 # We could also do
+###########################################################################
+# print((lambda x: x*3)(5))
+
+# is equal to
+def f(x):
+    return x * 3
+
+# print(f(5))
+###########################################################################
 
 def not_thirteen(x):
     return x != 13
 
 
-list(filter(not_thirteen, my_list))
+print(list(filter(not_thirteen, my_list)))
 
 # filter() passes each element of my_list as a parameter to the function.
 # Pretty neat, eh?
--- 12_Decorators.py
+++ 12_Decorators.py
@@ -1,44 +1,44 @@
 # A decorator is just a function that gets called before another function
 
 import functools  # function tools
-
-
-def my_decorator(f):
-    @functools.wraps(f)
-    def function_that_runs_f():
-        print("Hello!")
-        f()
-        print("After!")
-
-    return function_that_runs_f
-
-
-@my_decorator
-def my_function():
-    print("I'm in the function.")
-
-
-my_function()
+#
+#
+# def my_decorator(f):
+#     @functools.wraps(f)
+#     def function_that_runs_f():
+#         print("Hello!")
+#         f()
+#         print("After!")
+#
+#     return function_that_runs_f
+#
+#
+# @my_decorator
+# def my_function():
+#     print("I'm in the function.")
+#
+#
+# my_function()
 
 
 ###
 
-def my_decorator(f):
-    @functools.wraps(f)
-    def function_that_runs_f(*args, **kwargs):
-        print("Hello!")
-        f(*args, **kwargs)
-        print("After!")
-
-    return function_that_runs_f
-
-
-@my_decorator
-def my_function(arg1, arg2):
-    print(arg1 + arg2)
-
-
-my_function(56, 89)
+# def my_decorator(f):
+#     @functools.wraps(f)
+#     def function_that_runs_f(*args, **kwargs):
+#         print("Hello!")
+#         f(*args, **kwargs)
+#         print("After!")
+#
+#     return function_that_runs_f
+#
+#
+# @my_decorator
+# def my_function(arg1, arg2):
+#     print(arg1 + arg2)
+#
+#
+# my_function(56, 89)
 
 
 ###
