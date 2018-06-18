ed79d837895c998f6222574d84b9b862f5ff4e5c
Lynne Maher <lynnemaher007@gmail.com>
2018-06-18 09:13:25 -0400

    Finished Section 4: Item 54.Improving code and error control

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-# from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt import JWT, jwt_required, current_identity
 
 from security import authenticate, identity
 
@@ -11,39 +11,36 @@ api = Api(app)
 
 # jwt = JWT(app, authenticate, identity)
 #
-# items = []
+items = []
 
 
-class Student(Resource):
+class Item(Resource):
+    #     parser = reqparse.RequestParser()
+    #     parser.add_argument('price',
+    #                         type=float,
+    #                         required=True,
+    #                         help="This field cannot be left blank!"
+    #                         )
+    #
+    #     @jwt_required()
     def get(self, name):
-        return {'student':name}
+        item = next(filter(lambda x:x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+        # return {'item':next(filter(lambda x:x['name'] == name, items), None)}
+
+    def post(self, name):
+        if next(filter(lambda x:x['name'] == name, items), None) is not None:
+            return {'message':"An item with name '{}' already exists.".format(name)}
+
+        # data = Item.parser.parse_args()
+        data = request.get_json()
+
+        item = {'name':name, 'price':data['price']}
+        # item = {'name':name, 'price':12}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
-
-app.run(port=5000)
-
-# class Item(Resource):
-#     parser = reqparse.RequestParser()
-#     parser.add_argument('price',
-#                         type=float,
-#                         required=True,
-#                         help="This field cannot be left blank!"
-#                         )
-#
-#     @jwt_required()
-#     def get(self, name):
-#         return {'item':next(filter(lambda x:x['name'] == name, items), None)}
-#
-#     def post(self, name):
-#         if next(filter(lambda x:x['name'] == name, items), None) is not None:
-#             return {'message':"An item with name '{}' already exists.".format(name)}
-#
-#         data = Item.parser.parse_args()
-#
-#         item = {'name':name, 'price':data['price']}
-#         items.append(item)
-#         return item
 #
 #     @jwt_required()
 #     def delete(self, name):
@@ -64,13 +61,18 @@ app.run(port=5000)
 #         return item
 #
 #
-# class ItemList(Resource):
-#     def get(self):
-#         return {'items':items}
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
+
+
 #
 #
-# api.add_resource(Item, '/item/<string:name>')
-# api.add_resource(ItemList, '/items')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 # if __name__ == '__main__':
 #     app.run(debug=True)  # important to mention debug=True
+
+
+app.run(port=5000, debug=True)  # important to mention debug=True
